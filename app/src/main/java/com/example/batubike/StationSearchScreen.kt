c1fdf7424aa351b95bed0c772aed2c3bdebc0ffb
wyxiao <71328511+davidhsiaotw@users.noreply.github.com>
2023-09-23 12:04:09 +0800

    feat: create keyword list
    show keyword list when MySearchBar is focused
    keep keyword list shown while typing
    recommend keywords based on user's input

--- app/src/main/java/com/example/batubike/StationSearchScreen.kt
+++ app/src/main/java/com/example/batubike/StationSearchScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -25,11 +27,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,23 +47,33 @@ import kotlinx.coroutines.launch
 fun StationSearchScreen(modifier: Modifier) {
     var input by rememberSaveable { mutableStateOf("") }
     var filter by rememberSaveable { mutableStateOf("") }
+    var isSearchBarFocused by rememberSaveable { mutableStateOf(false) }
     Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(12.dp), contentAlignment = Alignment.Center
+            .padding(12.dp)
     ) {
         Column {
             Text(text = "")
             Text(
-                text = "站點資訊",
-                color = Color(171, 195, 62),
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
+                text = "站點資訊", color = Color(171, 195, 62),
+                fontSize = 18.sp, fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(12.dp))
-            MySearchBar(input, { input = it }, { filter = it })
+            MySearchBar(input, {
+                input = it
+                isSearchBarFocused = true   // keep keyword list shown when input changes
+            }, { filter = it }, { isSearchBarFocused = it })
             Spacer(modifier = Modifier.height(12.dp))
-            StationList(filter)
+            Box {
+                StationList(filter)
+                RecommendKeywordList(
+                    input = input, show = isSearchBarFocused,
+                    onClick = {
+                        input = it
+                        isSearchBarFocused = false
+                    })
+            }
         }
     }
 }
@@ -69,7 +81,8 @@ fun StationSearchScreen(modifier: Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MySearchBar(
-    input: String = "", inputChange: (String) -> Unit, onSearch: (String) -> Unit = {}
+    input: String = "", inputChange: (String) -> Unit, onSearch: (String) -> Unit = {},
+    onFocusChange: (Boolean) -> Unit = {}
 ) {
     var label by rememberSaveable { mutableStateOf("搜尋站點") }
     // https://www.geeksforgeeks.org/how-to-clear-focus-of-textfield-in-android-using-jetpack-compose/
@@ -87,8 +100,10 @@ private fun MySearchBar(
         modifier = Modifier
             .fillMaxWidth()
             .onFocusChanged {
-                if (it.isFocused) label = ""
-                else {
+                onFocusChange(it.isFocused)
+                if (it.isFocused) {
+                    label = ""
+                } else {
                     if (input.isEmpty())
                         coroutineScope.launch {
                             delay(150L) // wait for clearFocus() complete
@@ -115,6 +130,31 @@ private fun MySearchBar(
             unfocusedIndicatorColor = Color.Transparent
         )
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun RecommendKeywordList(input: String, show: Boolean, onClick: (String) -> Unit = {}) {
+    if (show) {
+        var defaultKeywords = taipeiAreas.filter {
+            it.matches("^.*$input.*$".toRegex())
+        }
+        if (defaultKeywords.size > 6) defaultKeywords = defaultKeywords.slice(0..5)
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(modifier = Modifier.clip(RoundedCornerShape(12.dp))) {
+            defaultKeywords.forEach { area ->
+                Card(
+                    onClick = { onClick(area) },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RectangleShape,
+                    colors = CardDefaults.cardColors(containerColor = Color(245, 245, 245))
+                ) {
+                    Text(text = area, modifier = Modifier.padding(12.dp), fontSize = 24.sp)
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -150,7 +190,7 @@ private fun StationList(input: String) {
                         Row(
                             Modifier
                                 .fillMaxWidth()
-                                .background(Color(164, 164, 164))
+                                .background(Color(245, 245, 245))
                         ) {
                             TableCell(text = "台北市")
                             TableCell(text = it.area)
@@ -167,22 +207,21 @@ private fun StationList(input: String) {
                         }
                         isWhite = true
                     }
-
-
                 }
-
             }
         }
     }
 }
 
 @Composable
-private fun TableCell(
-    text: String,
-    textColor: Color = Color.Black,
-) {
+private fun TableCell(text: String, textColor: Color = Color.Black) {
     Text(text = text, modifier = Modifier.padding(12.dp), color = textColor)
 }
+
+private val taipeiAreas: List<String> = listOf(
+    "松山區", "信義區", "大安區", "中山區", "中正區", "大同區", "萬華區", "文山區", "南港區",
+    "內湖區", "士林區", "北投區"
+)
 
 @Preview(showBackground = true)
 @Composable
@@ -190,5 +229,13 @@ private fun MySearchBarPreview() {
     BatUbikeTheme {
         var input by rememberSaveable { mutableStateOf("") }
         MySearchBar(input, { input = it })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RecommendKeywordListPreview() {
+    BatUbikeTheme {
+        RecommendKeywordList("", true)
     }
 }
